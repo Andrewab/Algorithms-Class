2f5c586848362e40aa8b50a4d536c82eabb9fd28
Andrewab <andrewalcideblanchette@gmail.com>
2021-09-29 01:58:09 -0700

    finalized hw

--- hw3/pq_starter/PriorityQueue.java
+++ hw3/pq_starter/PriorityQueue.java
@@ -25,7 +25,6 @@ public class PriorityQueue {
 
 	protected Map<Integer, Integer> location;
 	protected List<Pair<Integer, Integer>> heap;
-	private int highestPriority;
 
 	public static void main(String[] args) {
 
@@ -39,7 +38,6 @@ public class PriorityQueue {
 		// TODO: Fill in
 		this.location = new HashMap<>();
 		this.heap = new ArrayList<>();
-		this.highestPriority = Integer.MAX_VALUE;
 	}
 
 
@@ -62,10 +60,11 @@ public class PriorityQueue {
 		if(priority >= 0 && !this.location.containskey(element)) {
 			Pair<Integer, Integer> newPair = new Pair<Integer, Integer>(priority,element);
 			this.heap.add(newPair);
-			this.location.add(priority,element);
+			this.location.put(element, this.heap.indexOf(newPair));
+			percolateUp(heap.size() - 1);
 		}
 		else {
-			throw AssertionError("The priority is non-negative or duplicate.");
+			throw new AssertionError("Push Error: The element does not already appear in the priority queue");
 		}
 	}
 
@@ -80,6 +79,9 @@ public class PriorityQueue {
 	 */
 	public void pop(){
 		// TODO: Fill in
+		int elemToRemove = this.location.get(pushDown(topElement()));
+		this.heap.remove(elemToRemove);
+		this.location.remove(elemToRemove);
 	}
 
 
@@ -94,15 +96,7 @@ public class PriorityQueue {
 	 */
 	public int topPriority() {		
 		// TODO: Fill in
-
-		int i = 0;
-		while(true){
-
-			if (location.containsKey(i)){
-				return i;
-			}
-			i++;
-		}
+		return this.heap.get(0).priority;
 
 	}
 
@@ -118,7 +112,7 @@ public class PriorityQueue {
 	 */
 	public int topElement() {
 		// TODO: Fill in
-	return this.location.getvalue(topPriority());
+		return this.heap.get(0).element;
 	}
 
 
@@ -137,15 +131,11 @@ public class PriorityQueue {
 	 */
 	public void changePriority(int newpriority, int element) {
 		// TODO: Fill in
-			//if new priority is greater than zero and element current exists and that new priority is not already taken.
-		if (newpriority>=0 && isPresent(element) && !this.location.containsKey(newpriority)){
-
-			//delete current & replace with new priority
-			this.location.remove(getPriority(element));
-			push(newpriority, element);
-		}
-		else {
-			throw AssertionError("Changing priority failed the constraints");
+		if(newpriority >= 0 && isPresent(element) && !this.location.containsKey(newPriority)) {
+			this.heap.get(this.location.get(element)).priority = newpriority;
+			if(newpriority < highestPriority){
+				highestPriority = newpriority;
+			}
 		}
 	}
 
@@ -164,16 +154,13 @@ public class PriorityQueue {
 	//Talk to chambers! two hash map?
 	public int getPriority(int element) {
 		// TODO: Fill in
-		if (this.location.containsValue(element)) {
-			for (location.Entry<Integer, Integer> entry : this.location.entrySet()) {
-				if (entry.getValue() == element) {
-					return entry.getKey();
-				}
-			}
+		if(this.location.containsValue(element)) {
+			int returnVal = this.heap.get(this.location.get(element));
+			return returnVal.priority;
 		}
-
 		return -1;
 	}
+
 
 	/**
 	 *  Returns true if the priority queue contains no elements
@@ -181,7 +168,7 @@ public class PriorityQueue {
 	 */
 	public boolean isEmpty() {
 		// TODO: Fill in
-		return this.heap.isEmpty();
+		return this.location.isEmpty();
 	}
 
 	/**
@@ -190,7 +177,7 @@ public class PriorityQueue {
 	 */
 	public boolean isPresent(int element) {
 		// TODO: Fill in
-		return this.heap.contains(element);
+		return this.location.contains(element);
 	}
 
 	/**
@@ -199,6 +186,7 @@ public class PriorityQueue {
 	public void clear() {
 		// TODO: Fill in
 		this.heap.clear();
+		this.location.clear();
 	}
 
 	/**
@@ -224,6 +212,16 @@ public class PriorityQueue {
 	 */
 	private int pushDown(int start_index) {	
 		// TODO: Fill in
+		if(heap.get(left(start_index)).priority < heap.get(start_index).priority) {
+			swap(start_index, left(start_index));
+			return left(start_index);
+		}
+		if(heap.get(right(start_index)).priority < heap.get(start_index).priority) {
+			swap(start_index, right(start_index));
+			return right(start_index);
+		}
+		return null;
+
 	}
 
 	/**
@@ -232,8 +230,14 @@ public class PriorityQueue {
 	 * @return the index in the list where the element is finally stored
 	 */
 	private int percolateUp(int start_index) {
-		// TODO: Fill in
-	}
+		if(heap.get(start_index) > 0) {
+			if (heap.get(start_index).priority < heap.get(parent(start_index)).priority) {
+				swap(start_index, parent(start_index));
+			}
+			return parent(start_index);
+		}
+		return null;
+		}
 
 
 	/**
@@ -244,6 +248,12 @@ public class PriorityQueue {
 	 */
 	private void swap(int i, int j) {
 		// TODO: Fill in
+		Pair<Integer,Integer> tmp = heap.get(i);
+		heap.set(i, heap.get(j));
+		heap.set(j,tmp);
+		location.replace(tmp.element, j);
+		location.replace(heap.get(i).element, i);
+
 	}
 
 	/**
@@ -253,6 +263,8 @@ public class PriorityQueue {
 	 */
 	private int left(int parent) {
 		// TODO: Fill in
+		int leftChild = (2*parent)+1;
+		return leftChild;
 	}
 
 	/**
@@ -262,6 +274,8 @@ public class PriorityQueue {
 	 */
 	private int right(int parent) {
 		// TODO: Fill in
+		int rightChild = (2*parent)+2;
+		return rightChild;
 	}
 
 	/**
@@ -272,63 +286,67 @@ public class PriorityQueue {
 
 	private int parent(int child) {
 		// TODO: Fill in
+		int parent = (child-1)/2;
+		return parent;
+
 	}
 	
 
 	/*********************************************************
 	 * 	These are optional private methods that may be useful
 	 *********************************************************/
-	
-	
-	/**
-	 * Push down the root element
-	 * @return the index in the list where the element is finally stored
-	 */
-	private int pushDownRoot() {
-		// TODO: A one-line function that calls pushDown()
-	}
-
-	/**
-	 * Percolate up the last leaf in the heap, i.e. the most recently 
-	 * added element which is stored in the last slot in the list
-	 * 
-	 * @return the index in the list where the element is finally stored
-	 */
-	private int percolateUpLeaf(){
-		// TODO: A one-line function that calls percolateUp()
-	}
-	
-	/**
-	 * Returns true if element is a leaf in the heap
-	 * @param i index of element in heap
-	 * @return true if element is a leaf
-	 */
-	private boolean isLeaf(int i){
-		// TODO: Fill in
-	}
-
-	/**
-	 * Returns true if element has two children in the heap
-	 * @param i index of element in the heap
-	 * @return true if element in heap has two children
-	 */
-	private boolean hasTwoChildren(int i) {
-		// TODO: Fill in
-	}
-	
-	/**
-	 * Print the underlying list representation
-	 */
-	private void printHeap() {
-		// TODO: Fill in
-	}
-
-	/**
-	 * Print the entries in the location map
-	 */
-	private void printMap() {
-		// TODO: Fill in
-	}
+//
+//	/**
+//	 * Push down the root element
+//	 * @return the index in the list where the element is finally stored
+//	 */
+//
+//	private int pushDownRoot() {
+//		// TODO: A one-line function that calls pushDown()
+//		return 0;
+//	}
+//
+//	/**
+//	 * Percolate up the last leaf in the heap, i.e. the most recently
+//	 * added element which is stored in the last slot in the list
+//	 *
+//	 * @return the index in the list where the element is finally stored
+//	 */
+//	private int percolateUpLeaf(){
+//		// TODO: A one-line function that calls percolateUp()
+//	}
+//
+//	/**
+//	 * Returns true if element is a leaf in the heap
+//	 * @param i index of element in heap
+//	 * @return true if element is a leaf
+//	 */
+//	private boolean isLeaf(int i){
+//		// TODO: Fill in
+//	}
+//
+//	/**
+//	 * Returns true if element has two children in the heap
+//	 * @param i index of element in the heap
+//	 * @return true if element in heap has two children
+//	 */
+//	private boolean hasTwoChildren(int i) {
+//		// TODO: Fill in
+//	}
+//
+//	/**
+//	 * Print the underlying list representation
+//	 */
+//	private void printHeap() {
+//		// TODO: Fill in
+//	}
+//
+//	/**
+//	 * Print the entries in the location map
+//	 */
+//	private void printMap() {
+//		// TODO: Fill in
+//	}
 
 	
 }
